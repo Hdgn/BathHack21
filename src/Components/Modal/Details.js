d6f5468621f9e1f3fc2b2daebc04a63e98c72ecc
Michelle <hyh27@bath.ac.uk>
2021-04-11 01:29:37 +0100

    settings 2

--- src/Components/Modal/Details.js
+++ src/Components/Modal/Details.js
@@ -1,35 +1,45 @@
-import React from 'react'
+import React, { useState }from 'react'
 import { Button, Icon, Image, Modal } from 'semantic-ui-react'
 import { Form } from 'semantic-ui-react'
+import { Link } from "react-router-dom";
 
-const Details = () => {
+const Details = (props) => {
   const [open, setOpen] = React.useState(false)
   
-//   const onPomodoroChange = (value) => {
-//     props.setPomodoro(value)
-//   }
+  const onPomodoroChange = (value) => {
+      console.log()
+    props.setPomodoro(Number(value))
+  }
 
-//   const onBreakChange = (value) => {
-//     props.setBreak(value)
-//   }
+  const onBreakChange = (value) => {
+    props.setBreak(value)
+  }
 
-//   const onSave = () => {
-//     localStorage.setItem('showTimerIndicator', props.showTimerIndicator)
-//     localStorage.setItem('notification', props.notification)
-//     localStorage.setItem('ringerType', props.ringerType)
-//     localStorage.setItem('ringerVolume', props.ringerVolume)
-//     localStorage.setItem('pomodoro', props.pomodoro)
-//     localStorage.setItem('shortBreak', props.shortBreak)
-//     localStorage.setItem('longBreak', props.longBreak)
-//     localStorage.setItem('pomodoroGoal', props.pomodoroGoal)
-//   }
+  const onSave = () => {
+    localStorage.setItem('showTimerIndicator', props.showTimerIndicator)
+    localStorage.setItem('notification', props.notification)
+    localStorage.setItem('ringerType', props.ringerType)
+    localStorage.setItem('ringerVolume', props.ringerVolume)
+    localStorage.setItem('pomodoro', props.pomodoro)
+    localStorage.setItem('shortBreak', props.shortBreak)
+    localStorage.setItem('longBreak', props.longBreak)
+    localStorage.setItem('pomodoroGoal', props.pomodoroGoal)
+  }
+
+  const handleSubmit = () => {
+    return
+    }
+
+    const handleChange = () => {
+
+    }
 
   return (
     <Modal
       open={open}
       onClose={() => setOpen(false)}
       onOpen={() => setOpen(true)}
-      trigger={<Button>Scrolling Content Modal</Button>}
+      trigger={<Button>Change Pomodoro Settings</Button>}
     >
       <Modal.Header>Pomodoro Settings</Modal.Header>
       <Modal.Content image scrolling>
@@ -41,9 +51,9 @@ const Details = () => {
           </p>
           <Form>
             <Form.Group widths='equal'>
-            <Form.Field label='Time' control='input' / >
-            {/* <Form.Field label='Time' control='input' value={props.pomodoro} onChange={e => onPomodoroChange(e.target.value)} / > */}
-            <Form.Field label='Pomodoro Goal' control='select'>
+            {/* <Form.Field label='Time' control='input' / > */}
+            {/* <Form.Field label='Time' control='input' value={props.pomodoro} onChange={e => onPomodoroChange(e.target.value)} / >  */}
+            {/* <Form.Field label='Pomodoro Goal' control='select' onChange= {}>
                 <option value='1'>1</option>
                 <option value='2'>2</option>
                 <option value='3'>3</option>
@@ -51,8 +61,8 @@ const Details = () => {
                 <option value='5'>5</option>
                 <option value='6'>6</option>
                 <option value='7'>7</option>
-            </Form.Field>
-            <Form.Field label='Pomodoro Time' control='select' >
+            </Form.Field> */}
+            <Form.Field label='Pomodoro Time' control='select' onChange= {e => onPomodoroChange(e.target.value)}>
                 <option value='20'>20</option>
                 <option value='25'>25</option>
                 <option value='30'>30</option>
@@ -112,8 +122,11 @@ const Details = () => {
         </Modal.Description>
       </Modal.Content>
       <Modal.Actions>
+        <Button onClick={() => onSave()} primary>
+          Save <Icon name='chevron right' />  
+          </Button>
         <Button onClick={() => setOpen(false)} primary>
-          Proceed <Icon name='chevron right' />
+          Close <Icon name='chevron right' />
         </Button>
       </Modal.Actions>
     </Modal>
--- src/Components/SelectMusic.js
+++ src/Components/SelectMusic.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 // import { Timer } from '../Timer';
 
+
 const SelectMusic = (props) => {
 
     const handleSubmit = () => {
