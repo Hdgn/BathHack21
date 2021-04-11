c56925841431005c54aff03bda5c66870f05d071
lf639 <lf639@bath.ac.uk>
2021-04-11 11:05:58 +0100

    Forgot to save

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -8,17 +8,11 @@ const Menu = (props) => {
 
         <div style={{backgroundColor: 'black'}}
         class="ui menu">
-<<<<<<< HEAD
             {/* <div class="right menu" > */}
                 <button class="ui fluid button secondary">
                     <Link class="item" to="/Main" style={{textAlign: "center", color: 'white', fontWeight: 'bold'}}>Home</Link>
                 </button>
             {/* </div> */}
-=======
-            <div class="right menu" >
-                <Link class="item" to="/Main" style={{color: 'white', fontWeight: 'extra-bold'}}>Home</Link>
-            </div>
->>>>>>> 0cc09c6a142b00c8e1cd4368a81f19d675813326
             {props.children}
         </div>
     )
